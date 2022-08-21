2063754c683409f94049470d4fa2b59f5659289f
Kabir-Vora <87693049+Kabir-Vora@users.noreply.github.com>
2022-08-21 14:24:08 +1000

    Add files via upload

---
+++ main.js
@@ -0,0 +1,72 @@
+var SpeechRecognition = window.webkitSpeechRecognition;
+  
+var recognition = new SpeechRecognition();
+
+var Textbox = document.getElementById("textbox"); 
+
+function start()
+{
+    Textbox.innerHTML = ""; 
+    recognition.start();
+} 
+
+function save()
+{
+  //write code store the anchor tag element in a variable ,store the image src in a variable ,assign the image variable to href of anchor tag and write code to click the href
+  
+  
+  
+ }
+
+
+
+recognition.onresult = function(event) {
+
+ console.log(event); 
+
+var Content = event.results[0][0].transcript;
+
+    Textbox.innerHTML = Content;
+    console.log(Content);
+      if(Content =="take my selfie")
+      {
+        console.log("taking selfie --- ");
+        speak();
+      }
+}
+
+
+function speak(){
+    var synth = window.speechSynthesis;
+
+    speak_data = "Taking you Selfie in 5 seconds";
+
+    var utterThis = new SpeechSynthesisUtterance(speak_data);
+
+    synth.speak(utterThis);
+
+    Webcam.attach(camera);
+
+    setTimeout(function()
+    { 
+        take_selfie(); 
+    }, 5000);
+}
+
+ 
+camera = document.getElementById("camera");
+Webcam.set({
+    width:360,
+    height:250,
+    image_format : 'jpeg',
+    jpeg_quality:90
+});
+
+function take_selfie()
+{
+    Webcam.snap(function(data_uri) {
+        document.getElementById("result").innerHTML = '<img id="selfie_image" src="'+data_uri+'"/>';
+    });
+}
+
+
